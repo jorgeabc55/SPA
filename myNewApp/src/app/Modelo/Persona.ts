502f1d34e75c9698f4c1f2cf2076379a586216cd
Jorge Cardenas <jorgeabc5552819@gmail.com>
2021-03-11 17:19:09 -0500

    VERSION 1

---
+++ myNewApp/src/app/Modelo/Persona.ts
@@ -0,0 +1,14 @@
+export class Persona {
+    id:number;
+    nombres:string;
+    apellidos:string;
+	idtipodocumento:number;
+	documento:string;
+	lugarresidencia:string;
+	fechanacimineto:string;
+	email:string;
+	telefono:int;
+	usuario:string;
+	password:string;
+    
+}
